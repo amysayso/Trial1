fb171493ad4309d14e1dfeea23ad7ac98c9b144e
Amy Saysouriyosack <amysayso@Amys-MacBook-Pro.local>
2018-09-17 11:08:06 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tiral3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
